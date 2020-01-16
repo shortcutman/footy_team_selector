74617a7336aa99a21d34053e59c038c8bdfdd38f
Daniel Burke <danieljburke@me.com>
2020-01-16 12:15:21 +1100

    get some simple searching up

--- backend/resolvers.js
+++ backend/resolvers.js
@@ -1,18 +1,42 @@
 
+const csvtojson = require('csvtojson')
 
+const { ApolloError } = require('apollo-server-express')
+
+async function getPlayerData(filename) {
+	return await csvtojson().fromFile(filename)
+}
+
+const data = getPlayerData('./data/players.subset.csv')
 
 const resolvers = {
 	Query: {
-		getPlayerByNumber(parent, args) {
-			return {
-				id: 1111111111,
-				firstname: "test",
-				lastname: "test",
-				number: 1,
-				team: "GEELONG"
-			}
+		async getPlayerByNumber(parent, args) {
+			return data.then((playerData) => {
+				const element = playerData.find(el => el.number == args.number && el.team == args.team)
+				if (element) {
+					return element					
+				} else {
+					throw new ApolloError("Unable to find player", "NO_PLAYER", { input: args })
+				}
+			})
 		},
-		findPlayer(parent, args) {
+		async findPlayer(parent, args) {
+			const queries = args.query.split(" ").map(s => s.toLowerCase())
+
+			return data.then((playerData) => {
+				return playerData.filter((el) => {
+					for (const q of queries) {
+						let qNo = +q
+						if (isNaN(qNo)) {
+							return el.firstname.toLowerCase().includes(q) ||
+								   el.lastname.toLowerCase().includes(q)
+						} else {
+							return el.number == q
+						}
+					}
+				})
+			})
 		}
 	}
 }
--- backend/resolvers.test.js
+++ backend/resolvers.test.js
@@ -8,29 +8,195 @@ const { readFileSync } = require('fs')
 const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8')
 const resolvers = require('./resolvers.js')
 
-test('Get Player', async () => {
+let query;
+
+beforeAll(() => {
 	const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8')
 	const server = new ApolloServer({
 		typeDefs,
 		resolvers,
 	})
-	const { query } = createTestClient(server)
+	query = createTestClient(server).query
+})
 
-	const player = await query({
-		query: gql`query {
-			getPlayerByNumber(number:1 team:GEELONG) {
-				id
-				firstname
-				lastname
-				number
-				team
-			}
-		}`
+describe('getPlayerByNumber', () => {
+	test('Data structure', async () => {
+		const player = await query({
+			query: gql`query {
+				getPlayerByNumber(number:1 team:ADELAIDE) {
+					id
+					firstname
+					lastname
+					number
+					team
+				}
+			}`
+		})
+
+		expect(player.data.getPlayerByNumber).toHaveProperty("id")
+		expect(player.data.getPlayerByNumber).toHaveProperty("firstname")
+		expect(player.data.getPlayerByNumber).toHaveProperty("lastname")
+		expect(player.data.getPlayerByNumber).toHaveProperty("number")
+		expect(player.data.getPlayerByNumber).toHaveProperty("team")
 	})
 
-	expect(player.data.getPlayerByNumber).toHaveProperty("id")
-	expect(player.data.getPlayerByNumber).toHaveProperty("firstname")
-	expect(player.data.getPlayerByNumber).toHaveProperty("lastname")
-	expect(player.data.getPlayerByNumber).toHaveProperty("number")
-	expect(player.data.getPlayerByNumber).toHaveProperty("team")
+	test('Correct player', async () => {
+		const player = await query({
+			query: gql`query {
+				getPlayerByNumber(number:1 team:ADELAIDE) {
+					id
+					firstname
+					lastname
+					number
+					team
+				}
+			}`
+		})
+
+		expect(player.data.getPlayerByNumber).toMatchObject({
+			id: '993946',
+			firstname: 'Ben',
+			lastname: 'Keays',
+			team: 'ADELAIDE',
+			number: 1,
+		})
+	})
+
+	test('Missing player', async () => {
+		const player = await query({
+			query: gql`query {
+				getPlayerByNumber(number:50 team:ADELAIDE) {
+					id
+					firstname
+					lastname
+					number
+					team
+				}
+			}`
+		})
+		expect(player).toMatchObject({
+			errors: [{
+				path: ["getPlayerByNumber"],
+				extensions: {
+					input: {
+						number: 50,
+						team: "ADELAIDE"
+					},
+					code: "NO_PLAYER"
+				}
+			}]
+		})
+	})
+})
+
+describe('find player', () => {
+	test('Find by number', async () => {
+		const findQuery = await query({
+			query: gql`query {
+				findPlayer(query:"1") {
+					id
+					firstname
+					lastname
+					number
+					team
+				}
+			}`
+		})
+
+		expect(findQuery.data.findPlayer).toMatchObject([{
+			id: '993946',
+			firstname: 'Ben',
+			lastname: 'Keays',
+			team: 'ADELAIDE',
+			number: 1,
+		}])
+	})
+
+	test('Find by firstname', async () => {
+		const findQuery = await query({
+			query: gql`query {
+				findPlayer(query:"Ben") {
+					id
+					firstname
+					lastname
+					number
+					team
+				}
+			}`
+		})
+
+		expect(findQuery.data.findPlayer).toEqual(expect.arrayContaining([{
+			id: '993946',
+			firstname: 'Ben',
+			lastname: 'Keays',
+			team: 'ADELAIDE',
+			number: 1,
+		}]))
+	})
+
+	test('Find by lastname', async () => {
+		const findQuery = await query({
+			query: gql`query {
+				findPlayer(query:"Keays") {
+					id
+					firstname
+					lastname
+					number
+					team
+				}
+			}`
+		})
+
+		expect(findQuery.data.findPlayer).toEqual(expect.arrayContaining([{
+			id: '993946',
+			firstname: 'Ben',
+			lastname: 'Keays',
+			team: 'ADELAIDE',
+			number: 1,
+		}]))
+	})
+
+	test('Find by firstname mixed case', async () => {
+		const findQuery = await query({
+			query: gql`query {
+				findPlayer(query:"BeN") {
+					id
+					firstname
+					lastname
+					number
+					team
+				}
+			}`
+		})
+
+		expect(findQuery.data.findPlayer).toEqual(expect.arrayContaining([{
+			id: '993946',
+			firstname: 'Ben',
+			lastname: 'Keays',
+			team: 'ADELAIDE',
+			number: 1,
+		}]))
+	})
+
+	test('Find by lsatname mixed case', async () => {
+		const findQuery = await query({
+			query: gql`query {
+				findPlayer(query:"KeAyS") {
+					id
+					firstname
+					lastname
+					number
+					team
+				}
+			}`
+		})
+
+		expect(findQuery.data.findPlayer).toEqual(expect.arrayContaining([{
+			id: '993946',
+			firstname: 'Ben',
+			lastname: 'Keays',
+			team: 'ADELAIDE',
+			number: 1,
+		}]))
+	})
 })
