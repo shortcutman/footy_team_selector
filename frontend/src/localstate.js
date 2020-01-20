89922021a0da3b304c472ef22fe783a4fac7f5bc
Daniel Burke <danieljburke@me.com>
2020-01-20 21:24:30 +1100

    move reusable query into const

--- frontend/src/localstate.js
+++ frontend/src/localstate.js
@@ -1,5 +1,13 @@
 import gql from 'graphql-tag'
 
+const queryCurrentTeam = gql`{
+	currentTeam @client {
+		id
+		firstname
+		lastname
+	}
+}`
+
 export const typeDefs = gql`
 	type Team {
 		players: [Player]!
@@ -7,19 +15,14 @@ export const typeDefs = gql`
 
 	type Mutation {
 		addPlayerToTeam(player:Player!): Team!
+		removePlayerFromTeam(player:Player!): Team!
 	}
 `;
 
 const resolvers = {
 	Mutation: {
 		addPlayerToTeam: (_, {player}, {cache}) => {
-			const { currentTeam } = cache.readQuery({query: gql`{
-				currentTeam @client {
-					id
-					firstname
-					lastname
-				}
-			}`})
+			const { currentTeam } = cache.readQuery({query: queryCurrentTeam})
 			
 			if (currentTeam.length >= 18) {
 				return false
