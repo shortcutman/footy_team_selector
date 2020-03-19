3890859e8f3a328f417c9b966df8eeb095df0b30
Daniel Burke <danieljburke@me.com>
2020-03-19 17:24:19 +1100

    implemented pagination on search

--- frontend/src/components/searcher.vue
+++ frontend/src/components/searcher.vue
@@ -6,6 +6,8 @@
 	<div>
 		<ul>
 			<li v-for="result in findPlayer">{{ result.number }}: {{ result.firstname }} {{ result.lastname }} <input type="submit" name="add" value="Add" @click="addPlayer(result)" :disabled="!canAdd || inCurrentTeam(result)"></li>
+			<li v-if="nextCursor != -1" @click="showMoreSearchResults()">Click for more results</li>
+			<li v-else-if="query.length != 0">No more results</li>
 		</ul>
 	</div>
 </div>
@@ -21,6 +23,7 @@ export default {
 		return {
 			query: '',
 			findPlayer: [],
+			nextCursor: -1,
 			canAdd: true
 		}
 	},
@@ -41,12 +44,38 @@ export default {
 		},
 		inCurrentTeam(player) {
 			return teamUtilities.playerInTeam(player, this.currentTeam)
+		},
+		showMoreSearchResults() {
+			console.log(this.nextCursor)
+			if (this.nextCursor === -1) {
+				console.log('no more results')
+				return
+			}
+
+			this.$apollo.queries.findPlayer.fetchMore({
+				variables: {
+					q: this.query,
+					c: this.nextCursor
+				},
+				updateQuery(previousResult, { fetchMoreResult }) {
+					console.log(previousResult)
+					console.log(fetchMoreResult)
+
+					return {
+						findPlayer: {
+							__typename: 'FindResults',
+							nextCursor: fetchMoreResult.findPlayer.nextCursor,
+							results: [...previousResult.findPlayer.results, ...fetchMoreResult.findPlayer.results]
+						}
+					}
+				}
+			})
 		}
 	},
 	apollo: {
 		findPlayer: {
-			query: gql`query find($q:String!){
-				findPlayer(query:$q) {
+			query: gql`query find($q:String! $c:Int!){
+				findPlayer(query:$q cursor:$c) {
 					results {
 						id
 						firstname
@@ -58,9 +87,10 @@ export default {
 				}
 			}`,
 			variables() {
-				return { q: this.query }
+				return { q: this.query, c:0 }
 			},
 			update(data) {
+				this.nextCursor = data.findPlayer.nextCursor
 				return data.findPlayer.results
 			},
 			prefetch: false
